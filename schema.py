837947f7dd6d4d08bcd86a1ab5f32e3101f3a1b6
arti <loscoarti@gmail.com>
2024-04-15 14:50:37 +0200

    user model added and jwt support

--- schema.py
+++ schema.py
@@ -12,3 +12,22 @@ class NoteType:
 class NoteInput:
     name: str
     description: str
+
+
+@strawberry.input
+class RegisterInput:
+    name: str
+    email: str
+    password: str
+
+
+@strawberry.input
+class LoginInput:
+    email: str
+    password: str
+
+
+@strawberry.type
+class LoginType:
+    email: str
+    token: str
